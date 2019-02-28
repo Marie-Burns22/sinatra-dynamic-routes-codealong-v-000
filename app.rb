0c8f1a0c4a0ee34345d926fc42786c4943b66daf
Marie Burns <mcburns2222@gmail.com>
2019-02-28 04:40:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,5 +14,14 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
-
+  get "/goodbye/:name" do 
+    @user_name = params[:name]
+    "Goodbye, #{@user_name}."
+  end
+  
+  # get "/multiply/:num1/:num2" do
+  #   @numa = params[:num1]
+  #   @numb = params[:num2]
+  #   <%="#{numa} * #{numb}"%>
+  # end
 end
